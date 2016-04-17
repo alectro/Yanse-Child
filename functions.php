101e95044aec9cbd7189a0a5ca532aa7e472d511
Alejandro Urrutia <colorale@Alejandros-iMac-2.local>
2016-04-17 22:14:51 +0800

    Moved from Parent to Child
    Options
    Metaboxes
    CPT
    Fonts
    Dev
    Images
    SEO
    Welcome

--- functions.php
+++ functions.php
@@ -1,16 +1,13 @@
 <?php
-/*********************
-Enqueue the proper CSS
-if you use Sass.
-*********************/
 
+require_once('lib/custom-post-types.php');
 
-function yanse_child_enqueue_style()
-{
-	// foundation stylesheet
-	wp_register_style( 'yanse-child-foundation-stylesheet', get_stylesheet_directory_uri() . '/css/app.css', array(), '' );
-	wp_enqueue_style( 'yanse-child-foundation-stylesheet' );
-}
-add_action( 'wp_enqueue_scripts', 'yanse_child_enqueue_style' );
+require_once('lib/theme-options.php');
+
+require_once('lib/seo.php');
+
+require_once('lib/welcome.php');
+
+require_once('lib/metaboxes.php');
 
 ?>
