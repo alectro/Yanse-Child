296345e0c7c455677596d6e62e8908cfe062b2c6
Alejandro Urrutia <colorale@Alejandros-iMac-2.local>
2016-04-21 14:37:14 +0800

    New default page

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Default Page
+ * @package Yanse
+ * @since Yanse 0.1
+ */
+
+get_header(); ?>
+
+	<div id="content" role="main">
+		<div class="row">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template/content', 'page' ); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- row end -->
+	</div><!-- #content -->
+<?php get_footer(); ?>
