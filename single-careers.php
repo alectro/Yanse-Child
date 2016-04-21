3cd8a214dd5087fa927d121302200e7d1760624d
Alejandro Urrutia <colorale@Alejandros-iMac-2.local>
2016-04-21 18:02:03 +0800

    Restructure of pages, custom post types and single

--- single-careers.php
+++ single-careers.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'careers-single' ); ?>
+				<?php get_template_part( 'template/cpt/cpt', 'careers-single' ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
--- single-works.php
+++ single-works.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'works-single' ); ?>
+				<?php get_template_part( 'template/cpt/cpt', 'works-single' ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
